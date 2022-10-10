a10705c9e1bc1b12d72b5f3f1eee87223c67afdb
JaniR <janirekinen@gmail.com>
2022-10-10 21:29:12 +0300

    webtoken autentikointi

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+
+const jwt = require('jsonwebtoken');
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    console.log("token = "+token);
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
+      console.log(err)
+  
+      if (err) return res.sendStatus(403)
+  
+      req.user = user
+  
+      next()
+    })
+}
+
+//const dotenv = require('dotenv');
+//dotenv.config();
 
 var indexRouter = require('./routes/index');
 var bookRouter = require('./routes/book');
@@ -17,10 +40,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
+app.use('/login', loginRouter);
+app.use(authenticateToken);
 app.use('/', indexRouter);
 app.use('/book', bookRouter);
 app.use('/student', studentRouter);
 app.use('/user', userRouter);
-app.use('/login', loginRouter);
 
 module.exports = app;
--- database.js
+++ database.js
@@ -1,8 +1,5 @@
 const mysql = require('mysql');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'netuser',
-  password: 'netpass',
-  database: 'netdb'
-});
+const dotenv = require('dotenv');
+dotenv.config();
+const connection = mysql.createPool(process.env.SQL_SERVER);
 module.exports = connection;
